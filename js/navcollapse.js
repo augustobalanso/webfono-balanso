b2ddc9069a302ccf6961051d147785f4c4418bd9
Augusto Balanso <acb.1991@gmail.com>
2022-01-20 08:54:31 -0300

    borrados archivos innecesarios

--- js/navcollapse.js
+++
@@ -1,6 +0,0 @@
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-const togglerNav = document.querySelector(".fa-bars")
-
-togglerNav.onclick = () => {
-  document.querySelector(".header__nav").classList.toggle("active")
-}
